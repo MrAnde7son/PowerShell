3f02d9bf8885dfe66b1c55cdad1a2a5436e19df4
Zecured <mizrahi.itamar@gmail.com>
2016-09-19 14:02:55 +0300

    Add files via upload

---
+++ Get-GPODelegation.ps1
@@ -0,0 +1,37 @@
+﻿<#
+    Author: Itamar Mizrahi (@Zecured)
+    License: GNU v3
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+
+function Get-GPODelegation
+{
+<#
+    .SYNOPSIS
+        Finds users with write permissions on GPO objects.
+        Author: Itamar Mizrahi (@Zecured)
+        License: GNU v3
+        Required Dependencies: None
+        Optional Dependencies: None
+    .DESCRIPTION
+
+    .EXAMPLE 
+       
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory=$False, Position=0, ValueFromPipeline=$True)]
+        [string]$Domain = $env:USERDOMAIN
+
+    )
+
+    $gpolist = Get-GPO -All
+    foreach ($gpo in $gpolist)
+    {
+        Write-Host "GPO Name: " + $gpo.DisplayName
+        Get-GPPermissions -Guid $gpo.Id -All| Where-Object {$_.Permission -eq "GpoEditDeleteModifySecurity"}
+    }
+}
