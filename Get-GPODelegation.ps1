11f8d0248a684dce44e7e5875c4528f18c1365a9
Itamar <mizrahi.itamar@gmail.com>
2017-01-26 13:48:52 +0200

    Add files via upload

--- Get-GPODelegation.ps1
+++ Get-GPODelegation.ps1
@@ -1,5 +1,5 @@
 ﻿<#
-    Author: Itamar Mizrahi (@Zecured)
+    Author: Itamar Mizrahi (@MrAnde7son)
     License: GNU v3
     Required Dependencies: None
     Optional Dependencies: None
@@ -9,29 +9,81 @@ function Get-GPODelegation
 {
 <#
     .SYNOPSIS
-        Finds users with write permissions on GPO objects.
-        Author: Itamar Mizrahi (@Zecured)
+        Finds users with write permissions on GPO objects which may allow privilege escalation within the domain.
+
+        Author: Itamar Mizrahi (@MrAnde7son)
         License: GNU v3
         Required Dependencies: None
         Optional Dependencies: None
+
     .DESCRIPTION
 
+    .PARAMETER GPOName
+        The GPO display name to query for, wildcards accepted.  
+
+    .PARAMETER Domain
+
+    .PARAMETER DomainController
+
+    .PARAMETER ADSPath
+
+    .PARAMETER PageSize
+
     .EXAMPLE 
-       
-    #>
+        PS C:\> Get-GPODelegation
+        Returns all GPO delegations in current forest.
 
+    .EXAMPLE 
+        PS C:\> Get-GPODelegation -GPOName
+        Returns all GPO delegations on a given GPO.
+#>
     [CmdletBinding()]
-    param
-    (
-        [parameter(Mandatory=$False, Position=0, ValueFromPipeline=$True)]
-        [string]$Domain = $env:USERDOMAIN
+    Param (
+        [String]
+        $GPOName = '*',
 
+        [String]
+        $Domain,
+
+        [String]
+        $DomainController,
+
+        [String]
+        $ADSPath,
+
+        [ValidateRange(1,10000)] 
+        [Int]
+        $PageSize = 200
     )
 
-    $gpolist = Get-GPO -All
-    foreach ($gpo in $gpolist)
-    {
-        Write-Host "GPO Name: " + $gpo.DisplayName
-        Get-GPPermissions -Guid $gpo.Id -All| Where-Object {$_.Permission -eq "GpoEditDeleteModifySecurity"}
+    $Exclusions = @("SYSTEM","Domain Admins","Enterprise Admins","CREATOR OWNER")
+
+    if ($GPOName){
+
+    }
+
+    $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+    $DomainList = @($objForest.Domains)
+    $Domains = $DomainList | foreach { $_.name }
+    foreach ($Domain in $Domains) {
+        $strFilter = "(&(objectCategory=groupPolicyContainer)(displayname=$GPOName))"
+        $objDomain = New-Object System.DirectoryServices.DirectoryEntry
+        $objSearcher = New-Object System.DirectoryServices.DirectorySearcher
+        $objSearcher.SearchRoot = $objDomain
+        $objSearcher.Filter = $strFilter
+        $objSearcher.PageSize = $PageSize
+        $objSearcher.SearchScope = "Subtree"
+        $listGPO = $objSearcher.FindAll()
+        foreach ($gpo in $listGPO){
+            $ACL = (([ADSI]$gpo.path).ObjectSecurity).Access | ? {$_.ActiveDirectoryRights -match "Write" -and $_.AccessControlType -eq "Allow"}  | ? {$_.IdentityReference -notmatch "SYSTEM" -and $_.IdentityReference -notmatch "Enterprise Admins" -and $_.IdentityReference -notmatch "Domain Admins" -and $_.IdentityReference -notmatch "CREATOR OWNER"}
+            $GpoACL = New-Object psobject
+            $GpoACL | Add-Member Noteproperty 'ADSPath' $gpo.Properties.adspath
+            $GpoACL | Add-Member Noteproperty 'GPODisplayName' $gpo.Properties.displayname
+            $GpoACL | Add-Member Noteproperty 'IdentityReference' $ACL.IdentityReference
+            $GpoACL | Add-Member Noteproperty 'ActiveDirectoryRights' $ACL.ActiveDirectoryRights
+            $GpoACL
+        }
     }
 }
+
+
