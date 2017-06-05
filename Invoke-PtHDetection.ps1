815b1f71c782b71e8791a8e054ebc71c79b6247f
Itamar <mizrahi.itamar@gmail.com>
2017-06-05 17:50:41 +0300

    Add files via upload

---
+++ Invoke-PtHDetection.ps1
@@ -0,0 +1,44 @@
+﻿<#
+    Author: Itamar Mizrahi (@Zecured)
+    License: GNU v3
+    Required Dependencies: None
+    Optional Dependencies: None
+#>
+
+function Invoke-PtHDetection
+{
+<#
+    .SYNOPSIS
+        Returns a potentially Pth events through event log parsing. credit to @HackingDave for the detection technique.
+        
+        Author: Itamar Mizrahi (@Zecured)
+        License: GNU v3
+        Required Dependencies: None
+        Optional Dependencies: None
+    .DESCRIPTION
+
+    .PARAMETER 
+
+    .EXAMPLE Invoke-PtHDetection
+       
+    #>
+
+    [CmdletBinding()]
+    param
+    (
+
+    )
+
+    $pthEvents = @()
+    $Events = Get-WinEvent -FilterHashtable @{LogName='Security';'Id'=4624,4625}
+    foreach ($event in $Events){
+        $eventxml = ([xml]$event.ToXml()).Event.EventData.Data
+        $LogonType = $eventxml.GetValue(8).'#text'
+        $LogonProcessName = $eventxml.GetValue(9).'#text'
+        $KeyLength = $eventxml.GetValue(15).'#text'
+        if ($LogonType -eq 3 -and $LogonProcessName -eq 'NtLmSsp' -and $KeyLength -eq 0){
+            $pthEvents += $event
+        }
+    }
+    return $pthEvents
+}
