711cc67ba8e656fdd4e68d961a4c1401c44280fa
Itamar <mizrahi.itamar@gmail.com>
2017-06-26 14:58:49 +0300

    Delete Test-ADAuthentication.ps1

--- Test-ADAuthentication.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -assemblyname System.DirectoryServices.AccountManagement
-Function Test-ADAuthentication 
-{
-    param
-    ($Username,$Password)
-    (new-object DirectoryServices.Directoryentry "",$Username,$Password).psbase.name -ne $null
-}
